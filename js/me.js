5b0b426fd7e866abbfe6c3bda562f744610c9f1c
myproject-profassinol@ <rayhanakanda78@gmail.com>
2024-07-09 11:54:05 +0600

    ja

---
+++ js/me.js
@@ -0,0 +1,11 @@
+var button = document.getElementById("btn");
+var items = document.getElementById("item");
+
+button.onclick = clickButton;
+
+function clickButton(){
+    
+    items.css='display: block;';
+
+}
+// clickButton();
